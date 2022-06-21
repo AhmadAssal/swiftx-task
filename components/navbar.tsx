c7cfde1e421613d3936dcb35c2dc9e3403a8b237
AhmadAssal <ahmadnaderassal@gmail.com>
2022-06-21 23:56:32 +0200

    feat: add currency dropdown list

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "../styles/navbar.module.css";
 import { navBar } from "../graphql/queries";
 import { useQuery } from "@apollo/client";
-
+import utils from "../styles/utils.module.css";
 interface category {
   name: string;
 }
 
+interface Currency {
+  label: string;
+  symbol: string;
+}
+
 export const Navbar = () => {
   const { loading, error, data } = useQuery(navBar);
+  const [chosenCurrency, setChosenCurrency] = useState<string>("$");
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
   return (
     <nav className={styles.navbar}>
       {data?.categories?.map((item: category) => (
@@ -17,6 +26,20 @@ export const Navbar = () => {
         </a>
       ))}
       <img src="logo.png" alt="" className={styles.logo} />
+      <div className={utils.dropdown}>
+        <p className={utils.clickable}>{chosenCurrency}</p>
+        <div className={utils.dropdownContent}>
+          {data?.currencies?.map((currency: Currency) => (
+            <div
+              className={utils.currencyDiv}
+              onClick={() => setChosenCurrency(currency.symbol)}
+            >
+              <p className={utils.currency}>{currency.label}</p>
+              <p className={utils.currency}>{currency.symbol}</p>
+            </div>
+          ))}
+        </div>
+      </div>
       <img src="cart.png" className={styles.cart} />
     </nav>
   );
