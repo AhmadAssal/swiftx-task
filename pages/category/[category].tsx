dbc3cd5d431e67ddd77c098995a8075ce453db0d
AhmadAssal <ahmadnaderassal@gmail.com>
2022-06-22 21:53:04 +0200

    feat: create category page

---
+++ pages/category/[category].tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import { Navbar } from "../../components/navbar";
+import styles from "../../styles/Home.module.css";
+const Category: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <h1>category</h1>
+    </div>
+  );
+};
+
+export default Category;
