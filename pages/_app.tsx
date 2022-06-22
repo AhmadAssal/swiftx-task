ac98fcb165be396876fc5534d3e4d0127f138774
AhmadAssal <ahmadnaderassal@gmail.com>
2022-06-22 21:52:52 +0200

    feat: add navbar to base page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,12 @@ import type { AppProps } from "next/app";
 
 import { ApolloProvider } from "@apollo/client";
 import getApolloClient from "../apollo-client";
-
+import { Navbar } from "../components/navbar";
 function MyApp({ Component, pageProps }: AppProps) {
   const apollo = getApolloClient();
   return (
     <ApolloProvider client={apollo}>
+      <Navbar></Navbar>
       <Component {...pageProps} />
     </ApolloProvider>
   );
