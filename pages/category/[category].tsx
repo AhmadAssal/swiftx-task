a10f4fdf8032cfbc71613d3834a027ef6eb92d8a
AhmadAssal <ahmadnaderassal@gmail.com>
2022-06-22 22:10:35 +0200

    feat: retrieve category name from url

--- pages/category/[category].tsx
+++ pages/category/[category].tsx
@@ -3,10 +3,12 @@ import Head from "next/head";
 import Image from "next/image";
 import { Navbar } from "../../components/navbar";
 import styles from "../../styles/Home.module.css";
+import { useRouter } from "next/router";
 const Category: NextPage = () => {
+  const router = useRouter();
   return (
     <div className={styles.container}>
-      <h1>category</h1>
+      <h1>{router.query.category}</h1>
     </div>
   );
 };
