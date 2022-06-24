d6812bb4903bb189266c9171c68c4d4c38d3d17e
AhmadAssal <ahmadnaderassal@gmail.com>
2022-06-24 21:45:49 +0200

    feat: map products in category page

--- pages/category/[category].tsx
+++ pages/category/[category].tsx
@@ -4,11 +4,38 @@ import Image from "next/image";
 import { Navbar } from "../../components/navbar";
 import styles from "../../styles/Home.module.css";
 import { useRouter } from "next/router";
+import { useQuery } from "@apollo/client";
+import { productsByCategory } from "../../graphql/queries";
+import { Item } from "../../components/Item";
+interface Product {
+  name: string;
+  gallery: string[];
+  inStock: boolean;
+}
+
 const Category: NextPage = () => {
   const router = useRouter();
+  const categorySlug = router.query.category;
+  const input = { input: { title: categorySlug } };
+  const { loading, error, data } = useQuery(productsByCategory, {
+    variables: input,
+  });
   return (
     <div className={styles.container}>
       <h1>{router.query.category}</h1>
+      {loading ? (
+        <h2>Loading data...</h2>
+      ) : data ? (
+        data.category.products.map((product: Product) => (
+          <Item
+            imageLink={product.gallery[0]}
+            itemName={product.name}
+            price={"23"}
+          ></Item>
+        ))
+      ) : (
+        <h1>There are no products in this category.</h1>
+      )}
     </div>
   );
 };
