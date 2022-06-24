9d90d2e4fa545bb57d1a17cebbe810b861f0022d
AhmadAssal <ahmadnaderassal@gmail.com>
2022-06-24 21:59:07 +0200

    feat: import currency state in category page

--- pages/category/[category].tsx
+++ pages/category/[category].tsx
@@ -4,8 +4,12 @@ import Image from "next/image";
 import { Navbar } from "../../components/navbar";
 import styles from "../../styles/Home.module.css";
 import { useRouter } from "next/router";
+import { currency } from "../../state/currency";
+import { useRecoilState } from "recoil";
+
 const Category: NextPage = () => {
   const router = useRouter();
+  const [chosenCurrency, setChosenCurrency] = useRecoilState(currency);
   return (
     <div className={styles.container}>
       <h1>{router.query.category}</h1>
