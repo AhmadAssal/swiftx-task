6b6088a5b7df161e435416f24ed85cec2def6570
AhmadAssal <ahmadnaderassal@gmail.com>
2022-06-25 12:01:52 +0200

    feat: add grid container to category page

--- pages/category/[category].tsx
+++ pages/category/[category].tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Navbar } from "../../components/navbar";
 import styles from "../../styles/Home.module.css";
+import categoryStyles from "../../styles/category.module.css";
 import { useRouter } from "next/router";
 import { useQuery } from "@apollo/client";
 import { productsByCategory } from "../../graphql/queries";
@@ -31,7 +32,9 @@ const Category: NextPage = () => {
   const [chosenCurrency, setChosenCurrency] = useRecoilState(currency);
   const categorySlug = router.query.category;
   const input = {
-    title: categorySlug,
+    input: {
+      title: categorySlug,
+    },
   };
   const { loading, error, data } = useQuery(productsByCategory, {
     variables: input,
@@ -40,26 +43,28 @@ const Category: NextPage = () => {
   return (
     <div className={styles.container}>
       <h1>{router.query.category}</h1>
-      {loading ? (
-        <h2>Loading data...</h2>
-      ) : data ? (
-        data.category.products.map((product: Product) => {
-          const prices = product.prices.filter(
-            (price: Price) => price.currency.symbol === chosenCurrency
-          );
-          const price = prices[0].amount + prices[0].currency.symbol;
-          return (
-            <Item
-              key={product.name}
-              imageLink={product.gallery[0]}
-              itemName={product.name}
-              price={price}
-            ></Item>
-          );
-        })
-      ) : (
-        <h1>There are no products in this category.</h1>
-      )}
+      <div className={categoryStyles.grid}>
+        {loading ? (
+          <h2>Loading data...</h2>
+        ) : data ? (
+          data.category.products.map((product: Product) => {
+            const prices = product.prices.filter(
+              (price: Price) => price.currency.symbol === chosenCurrency
+            );
+            const price = prices[0].amount + prices[0].currency.symbol;
+            return (
+              <Item
+                key={product.name}
+                imageLink={product.gallery[0]}
+                itemName={product.name}
+                price={price}
+              ></Item>
+            );
+          })
+        ) : (
+          <h1>There are no products in this category.</h1>
+        )}
+      </div>
     </div>
   );
 };
