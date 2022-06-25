5c0783af15cdb6f9a9dc341138ba52ee8e8b076d
AhmadAssal <ahmadnaderassal@gmail.com>
2022-06-25 11:46:06 +0200

    feat: implement currency changer

--- pages/category/[category].tsx
+++ pages/category/[category].tsx
@@ -10,10 +10,20 @@ import { Item } from "../../components/Item";
 import { currency } from "../../state/currency";
 import { useRecoilState } from "recoil";
 
+interface Currency {
+  label: string;
+  symbol: string;
+}
+
+interface Price {
+  currency: Currency;
+  amount: number;
+}
 interface Product {
   name: string;
   gallery: string[];
   inStock: boolean;
+  prices: Price[];
 }
 
 const Category: NextPage = () => {
@@ -26,20 +36,27 @@ const Category: NextPage = () => {
   const { loading, error, data } = useQuery(productsByCategory, {
     variables: input,
   });
+
   return (
     <div className={styles.container}>
       <h1>{router.query.category}</h1>
       {loading ? (
         <h2>Loading data...</h2>
       ) : data ? (
-        data.category.products.map((product: Product) => (
-          <Item
-            key={product.name}
-            imageLink={product.gallery[0]}
-            itemName={product.name}
-            price={"23"}
-          ></Item>
-        ))
+        data.category.products.map((product: Product) => {
+          const prices = product.prices.filter(
+            (price: Price) => price.currency.symbol === chosenCurrency
+          );
+          const price = prices[0].amount + prices[0].currency.symbol;
+          return (
+            <Item
+              key={product.name}
+              imageLink={product.gallery[0]}
+              itemName={product.name}
+              price={price}
+            ></Item>
+          );
+        })
       ) : (
         <h1>There are no products in this category.</h1>
       )}
