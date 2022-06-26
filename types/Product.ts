0052c49ef7a8b61f2e68375acb3fdb6557c1fb17
AhmadAssal <ahmadnaderassal@gmail.com>
2022-06-26 23:47:46 +0200

    feat: update product type

--- types/Product.ts
+++ types/Product.ts
@@ -1,7 +1,21 @@
 import { Price } from "./Price";
+
+export interface Item {
+  displayValue: string;
+  value: string;
+}
+export interface Attribute {
+  name: string;
+  type: string;
+  items: Item[];
+}
 export interface Product {
   name: string;
   gallery: string[];
   inStock: boolean;
   prices: Price[];
+  description: string;
+  category: string;
+  brand: string;
+  attributes: Attribute[];
 }
