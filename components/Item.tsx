0e580c8b3b3686f7dd1dea26745b538ffa272567
AhmadAssal <ahmadnaderassal@gmail.com>
2022-06-27 22:26:40 +0200

    feat: make items clickable

--- components/Item.tsx
+++ components/Item.tsx
@@ -1,27 +1,31 @@
 import React from "react";
 import styles from "../styles/Item.module.css";
+import Link from "next/link";
 
 interface ItemProps {
+  id: string;
   imageLink: string;
   itemName: string;
   price: string;
 }
 
-export const Item = ({ imageLink, itemName, price }: ItemProps) => {
+export const Item = ({ imageLink, itemName, price, id }: ItemProps) => {
   return (
-    <div className={styles.item}>
-      <img className={styles.image} src={imageLink} />
-      <img
-        src="/cart_icon.png"
-        className={styles.cartIcon}
-        onClick={() => {}}
-      />
-      <div className={styles.cardInfo}>
-        <h3 className={styles.itemTitle}>{itemName}</h3>
-        <b>
-          <p className={styles.itemPrice}>{price}</p>
-        </b>
+    <Link href={"/product/" + id}>
+      <div className={styles.item}>
+        <img className={styles.image} src={imageLink} />
+        <img
+          src="/cart_icon.png"
+          className={styles.cartIcon}
+          onClick={() => {}}
+        />
+        <div className={styles.cardInfo}>
+          <h3 className={styles.itemTitle}>{itemName}</h3>
+          <b>
+            <p className={styles.itemPrice}>{price}</p>
+          </b>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
