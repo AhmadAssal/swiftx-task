9860eced55819c59966bf8fd5d5ddd9e6fbd1fcb
AhmadAssal <ahmadnaderassal@gmail.com>
2022-07-01 16:12:24 +0200

    feat: highlight chosen attributes

--- components/CartItem.tsx
+++ components/CartItem.tsx
@@ -8,6 +8,7 @@ import { useRecoilState } from "recoil";
 import { currency } from "../state/currency";
 import { Item } from "../types/Item";
 import cartStyles from "../styles/cart.module.css";
+import attributeStyles from "../styles/attribute.module.css";
 
 interface CartItemProps {
   brand: string;
@@ -15,6 +16,7 @@ interface CartItemProps {
   prices: Price[];
   attributes: Attribute[];
   gallery: string[];
+  chosenAttribute: any;
 }
 
 export const CartItem = ({
@@ -23,6 +25,7 @@ export const CartItem = ({
   prices,
   attributes,
   gallery,
+  chosenAttribute,
 }: CartItemProps) => {
   const [amount, setAmount] = useState(1);
   const [chosenCurrency, setChosenCurrency] = useRecoilState(currency);
@@ -35,27 +38,36 @@ export const CartItem = ({
       <h2>{itemName}</h2>
       <h3>{price[0].amount + price[0].currency.symbol}</h3>
       <div>
-        {attributes.map((attribute: Attribute) => {
-          return (
-            <div>
-              {/* <h2>{attribute.name}</h2> */}
-              {attribute.items.map((item: Item) => (
-                <AttributeButton
-                  option={item.displayValue}
-                  onClick={() => {
-                    const name = attribute.name;
-                    const value = item.value;
-                    //   setChosenAttribute({
-                    //     ...chosenAttribute,
-                    //     [name]: value,
-                    //   }
-                    //   );
-                  }}
-                ></AttributeButton>
-              ))}
-            </div>
-          );
-        })}
+        {attributes ? (
+          attributes.map((attribute: Attribute) => {
+            return (
+              <div>
+                {/* <h2>{attribute.name}</h2> */}
+                {attribute.items.map((item: Item) => {
+                  let chosen = false;
+                  if (item.value === chosenAttribute[attribute.name])
+                    chosen = true;
+                  return (
+                    <AttributeButton
+                      option={item.displayValue}
+                      className={chosen ? attributeStyles.chosen : ""}
+                      onClick={() => {
+                        const name = attribute.name;
+                        const value = item.value;
+                        // setChosenAttribute({
+                        //   ...chosenAttribute,
+                        //   [name]: value,
+                        // });
+                      }}
+                    ></AttributeButton>
+                  );
+                })}
+              </div>
+            );
+          })
+        ) : (
+          <></>
+        )}
       </div>
     </div>
   );
