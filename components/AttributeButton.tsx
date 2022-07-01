b1f54543c751b71dd7ff2cf05c9c9e019a1431e6
AhmadAssal <ahmadnaderassal@gmail.com>
2022-07-01 16:11:59 +0200

    feat: pass className to attribute button

--- components/AttributeButton.tsx
+++ components/AttributeButton.tsx
@@ -3,10 +3,15 @@ import styles from "../styles/attribute.module.css";
 interface AttributeProps {
   option: string;
   onClick: () => void;
+  className: string;
 }
-export const AttributeButton = ({ onClick, option }: AttributeProps) => {
+export const AttributeButton = ({
+  onClick,
+  option,
+  className,
+}: AttributeProps) => {
   return (
-    <button className={styles.attribute} onClick={onClick}>
+    <button className={`${styles.attribute} ${className}`} onClick={onClick}>
       {option}
     </button>
   );
